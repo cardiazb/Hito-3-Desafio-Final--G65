5e5e1889d436fcce31b2fb89db786679b222c495
cardiazb <100144287+cardiazb@users.noreply.github.com>
2024-11-23 10:40:00 -0500

    Update secretKey.js

--- Config/secretKey.js
+++ Config/secretKey.js
@@ -2,8 +2,13 @@
 
 const SECRET_KEY = 'a0c7aacd71661bb8b52ee4acac19b8ec6304597a76ada08c39e6348f5d000000';
 const DB_CONFIG = {
-  postgresql://pizzeria_bd_c2ru_user:RRaWdy9braEsHyq2qKPTnjDozv26ftFS@dpg-ct0uq9jtq21c73ejfci0-a.oregon-postgres.render.com/pizzeria_bd_c2ru?ssl=true
-};
+  user: 'pizzeria_bd_c2ru_user',
+  password: 'RRaWdy9braEsHyq2qKPTnjDozv26ftFS',
+  database: 'pizzeria_bd_c2ru',
+  host: 'dpg-ct0uq9jtq21c73ejfci0-a.oregon-postgres.render.com',
+  port: 5432,
+  ssl: true
+  };
 const SALT_ROUNDS = 10;
 
 const TokenPrueba = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhcmxvc2RpYXpiZWNhcjJAZ21haWwuY29tIiwidGlwbyI6IkNsaWVudGUiLCJpZCI6IjIzIiwibm9tYnJlIjoiQ2FybG9zIiwiYXBlbGxpZG8iOiJEaWF6IiwiaWF0IjoxNzMxODc2MjcwLCJleHAiOjE3MzI0ODEwNzB9.3hesx2s0wciOOISaSBxcZLS4pRzl8jktQFrhS5SBGeU';
